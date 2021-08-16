2a1b46a82c43f32ab5ee1dd8785bc7847f649f4f
Harrison Cramer <harrisoncramer@gmail.com>
2021-08-16 14:31:41 -0700

    registered sw

---
+++ index.js
@@ -0,0 +1,21 @@
+const sw_log = (msg) => console.log(`%c ${msg}`, "color: green; font-weight: bold");
+
+export const register = () => {
+    if (navigator.serviceWorker) {
+        navigator.serviceWorker.register('./sw.js')
+        .then(_ => {
+            sw_log("Service worker registered.");
+        })
+        .catch((err) => {
+          sw_log('Service worker not registered.');
+          console.log(err);
+        });
+      } else {
+        sw_log('Service workers are not possible on this browser.');
+      }    
+}
+
+/*
+export const installing = () => {
+
+}*/
