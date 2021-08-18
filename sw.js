a8a26384e11dee438964089ecc00b7f96fc1bc3a
Harrison Cramer <harrisoncramer@gmail.com>
2021-08-18 14:33:10 -0700

    Added comments, refactored code. Yay! It works!

--- sw.js
+++ sw.js
@@ -1,11 +1,18 @@
+import { get, set } from 'idb-keyval';
+import CryptoJS from 'crypto-js';
+
+// e.request is a stream, so we have to
+// read every chunk, write to a string and return it
 const getBody = async (e) => {
   const blob = await e.request.blob();
   const body = await blob.text();
   return body;
 };
 
+// Listen for fetch events, and for those to the /graphql endpoint,
+// run our caching logic, passing in information about the request.
 self.addEventListener('fetch', async (fetchEvent) => {
-  const { url, method, headers } = fetchEvent.request; // headers is an iterable...
+  const { url, method, headers } = fetchEvent.request;
   if (url.endsWith('/graphql')) {
     const body = await getBody(fetchEvent);
     const queryResult = await runCachingLogic(url, method, headers, body);
@@ -13,24 +20,53 @@ self.addEventListener('fetch', async (fetchEvent) => {
   }
 });
 
+// The main wrapper function for our caching solution
 async function runCachingLogic(url, method, headers, body) {
-  // Hash the query
-  // Check whether the query exists in our DB
-    // If it does, return the value
-  // If not, execute the query (Jae + Harry) and return result into variable
-  const result = await executeQuery(url, method, headers, body);
-  console.log('result from query =', result);
-  // Set the hash and the result in indexDB
-  // Return the result to the user
+  const hashedQuery = hashQuery(body);
+  const cachedData = await checkQueryExists(hashedQuery);
+  if (cachedData) {
+    console.log('Fetched from cache');
+    console.log(cachedData);
+    return cachedData;
+  } else {
+    const data = await executeQuery(url, method, headers, body);
+    writeToCache(hashedQuery, data);
+    return data;
+  }
 }
 
-async function executeQuery(url, method, headers, body) { 
-  return fetch(url, { method, headers, body })
-    .then(r => r.json())
-    .then(data => {
-      return data
-    })
-    .catch(err => {
-      console.error("ERROR IN SW FETCH: ", err);
-    })
+// Hash the query and convert to hex string
+function hashQuery(clientQuery) {
+  const hashedQuery = CryptoJS.MD5(JSON.stringify(clientQuery));
+  return hashedQuery.toString(CryptoJS.enc.hex);
+}
+
+// Checks for existence of hashed query in IDB
+async function checkQueryExists(hashedQuery) {
+  console.log(hashedQuery);
+  try {
+    const val = await get(hashedQuery);
+    return val;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// If the query doesn't exist in the cache, then execute
+// the query and return the result.
+async function executeQuery(url, method, headers, body) {
+  try {
+    const response = await fetch(url, { method, headers, body });
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// Write the result into cache
+function writeToCache(hash, queryResult) {
+  set(hash, queryResult)
+    .then(() => console.log('It worked!'))
+    .catch((err) => console.log('It failed!', err));
 }
