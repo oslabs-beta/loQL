91d880675c4446ea8b329d5d047af47599479e3b
Konstantin <konst@ntinh.am>
2021-08-18 16:42:30 -0400

    added standalone hash function and writeToDb function

--- sw.js
+++ sw.js
@@ -48,3 +48,15 @@ async function executeQuery(url, method, headers, body) {
       console.error("ERROR IN SW FETCH: ", err);
     })
 }
+
+async function hashQuery(clientQuery) {
+    //convert client query to string and run md5 hash function on it
+    const hash = CryptoJS.MD5(JSON.stringify(clientQuery));
+    return hash.toString(CryptoJS.enc.hex);
+  }
+
+async function writeToCache(hash, queryResult) {
+    set(hashQuery(hash), queryResult)
+        .then(() => console.log('It worked!'))
+        .catch((err) => console.log('It failed!', err));
+}
