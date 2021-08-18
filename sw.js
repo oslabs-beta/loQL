3d4c4d26a5f4491e8d5ae1bb746aa2f5af7953a2
Jae Ryu <thejaeryu@gmail.com>
2021-08-18 15:17:51 -0400

    executeQuery logic done

--- sw.js
+++ sw.js
@@ -1,4 +1,3 @@
-
 const getBody = async (e) => {
   const blob = await e.request.blob();
   const body = await blob.text();
@@ -14,48 +13,24 @@ self.addEventListener('fetch', async (fetchEvent) => {
   }
 });
 
-async function runCachingLogic(url, method, headers, body) {}
+async function runCachingLogic(url, method, headers, body) {
+  // Hash the query
+  // Check whether the query exists in our DB
+    // If it does, return the value
+  // If not, execute the query (Jae + Harry) and return result into variable
+  const result = await executeQuery(url, method, headers, body);
+  console.log('result from query =', result);
+  // Set the hash and the result in indexDB
+  // Return the result to the user
+}
 
-async function storeQueryResult(arg) {
-  const hashedQuery = function KonstantinHash(/* getbody result */);
-
-  // check if that query key exists in indexDB
-  const req = objectStore.openCursor(hashedQuery);
-
-  req.onsuccess = function(e) {
-    const cursor = e.target.result;
-    if (!cursor) {
-      // if key does not exist
-      // let the original fetch from client execute to GraphQL server
-      // change this later from hard coded
-      fetch('/api/graphql', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-        },
-        body: JSON.stringify({
-            query: `
-            query GetHumanQuery($id: String!) {
-                human(input: {id: $id }) {
-                  id
-                }
-              }
-            `,
-            variables: {
-                id: "jBWMVGjm50l5LGwepDoty"
-            }
-        })
-      })
-        .then(r => r.json())
-        .then(data => {
-          // store this response data to indexDB at that cached query key
-          console.log('response from query:', data)
-          console.log('hashed query key to store to =', hashedQuery);
-          objectStore.add(data, hashedQuery);
-        });
-    } else {
-      // if key already exists --> might not need this
-    }
-  }
+async function executeQuery(url, method, headers, body) { 
+  return fetch(url, { method, headers, body })
+    .then(r => r.json())
+    .then(data => {
+      return data
+    })
+    .catch(err => {
+      console.error("ERROR IN SW FETCH: ", err);
+    })
 }
