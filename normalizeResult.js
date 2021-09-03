8616da8c4a6b7749f24ec4f0dba8812c38a0c1ec
Jae Ryu <thejaeryu@gmail.com>
2021-09-03 11:57:53 -0400

    normlizeresult working

--- normalizeResult.js
+++ normalizeResult.js
@@ -1,49 +1,70 @@
 // this assumes results at top level comes in as an array
 // NODE FIX THIS : make sure it accounts for if just object
+const rootQueryObject = {};
+const denestedObjects = [];
+
+/* If top level result is an array, denest every object in it.
+* If it's an object, start denesting immediately.
+*/
 
 export function normalizeResult(result) {
-  const rootQueryObject = {};
-  const denestedObjects = [];
+ for (const key in result) {
+   rootQueryObject[key] = `loql__${key}`;
+   const value = result[key];
+   if (Array.isArray(value)) {
+     rootQueryObject[key] = flattenArray({ value, childKey: key });
+   } else {
+     flatObjAndWrite({ object: result[key], parentKey: key });
+   }
+ }
 
-  for (const key in result) {
-    rootQueryObject[key] = `loql__${key}`;
-    recurse({ data: result[key], parentKey: key });
-  }
-
-  function recurse({ data, parentKey }) {
-    const result = {};
-    for (const key in data) {
-      const value = data[key];
-      if (Array.isArray(value)) {
-        let childKey = parentKey + '_' + key;
-        result[key] = value.map((subArrayVal) => {
-          if (typeof subArrayVal === 'object' && subArrayVal !== null) {
-            if (!subArrayVal.id) return subArrayVal; // maybe return undefined
-            const uniqueKey = childKey + '_' + subArrayVal.id;
-            return recurse({ data: subArrayVal, parentKey: uniqueKey });
-          }
-          return subArrayVal;
-        });
-
-        // If undefined: Any key which has an array containing undefined values should be deleted from the object...?
-      } else if (typeof value === 'object' && value !== null) {
-        let childKey = parentKey + '_' + key;
-        result[key] = recurse({ data: value, parentKey: childKey });
-      } else {
-        result[key] = value;
-      }
-    }
-
-    if (rootQueryObject[parentKey]) {
-      denestedObjects.push({ [parentKey]: Object.values(result) });
-    } else {
-      denestedObjects.push({ [parentKey]: result });
-    }
-    return 'loql__' + parentKey;
-  }
-
-  return { rootQueryObject, denestedObjects };
+ return { rootQueryObject, denestedObjects };
 }
+
+/*
+* Takes an object, and a parent key. Recursively goes through keys
+* in object, and writes all to denestedObjects array. If result at key is a
+* primitive, write to result. If object, set to result of recursive call.
+* If array, set to mapped value of flattenedArray.
+*/
+function flatObjAndWrite({ object, parentKey }) {
+ const result = {};
+ for (const key in object) {
+   const value = object[key];
+   if (Array.isArray(value)) {
+     let childKey = parentKey + '_' + key;
+     result[key] = flattenArray({ value, childKey });
+   } else if (typeof value === 'object' && value !== null) {
+     let childKey = parentKey + '_' + key;
+     result[key] = flatObjAndWrite({ object: value, parentKey: childKey });
+   } else {
+     result[key] = value;
+   }
+ }
+
+ denestedObjects.push({ [parentKey]: result });
+ return 'loql__' + parentKey;
+}
+
+/*
+* Takes an array, loops over every value in the array.
+* For every object, calls flattenObject, then returns the ref.
+* For every non-object, returns the value directly.
+*/
+function flattenArray({ value, childKey }) {
+ return value.map((subArrayVal) => {
+   if (typeof subArrayVal === 'object' && subArrayVal !== null) {
+     if (!subArrayVal.id) return subArrayVal;
+     const uniqueKey = childKey + '_' + subArrayVal.id; // Add id...
+     return flatObjAndWrite({
+       object: subArrayVal,
+       parentKey: uniqueKey,
+     });
+   }
+   return subArrayVal;
+ });
+}
+
 
 // /* 
 //   Accepts the result from the GQL server and de-nests the object, 
