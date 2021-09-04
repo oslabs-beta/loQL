dcd596e4c243d1007be026a34615aead3669f339
Harrison Cramer <harrisoncramer@gmail.com>
2021-09-04 14:59:37 -0700

    Added basic testing files.

--- __tests__/doNotCacheCheck.js
+++
@@ -1,5 +0,0 @@
-import { defaultSettings } from '../metrics';
-
-test('Testing stuff', () => {
-  expect(1 + 1).toBe(2);
-});
---
+++ __tests__/md5.test.js
@@ -0,0 +1,9 @@
+import { ourMD5 } from '../md5';
+
+xdescribe('The hashing function.', () => {
+  test('Function should hash string to the same result each time.', () => {
+    const result = ourMD5('This is a long string that should be hashed!');
+    const result2 = ourMD5('This is a long string that should be hashed!');
+    expect(result).toEqual(result2);
+  });
+});
