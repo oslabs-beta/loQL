b46da1a3118c670e906c1d5584791a77e10cb41d
Harrison Cramer <harrisoncramer@gmail.com>
2021-09-09 13:48:02 -0700

    removed normalizeCache function for production deploy

--- __tests__/normalizeResult.test.js
+++
@@ -1,7 +0,0 @@
-import { normalizeResult } from '../helpers/normalizeResult';
-
-xdescribe('Normalizer.', () => {
-  test('Function should successfully normalize single object', () => {
-    const result = normalizeResult({ one: 1, two: 2, three: 3 });
-  });
-});
